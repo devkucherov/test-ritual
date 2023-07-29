d9534a662cf11eb4c377794471704d3f962b0e94
Pasha Kucherov <dev.kucherov@gmail.com>
2023-07-29 14:55:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  parallax: true,
+  speed: 1000,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+
+  // // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
