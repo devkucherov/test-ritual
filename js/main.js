5c6336b5362daaf7a918287b59f306efb9e6db83
Pasha Kucherov <dev.kucherov@gmail.com>
2023-08-03 20:51:06 +0300

    пофиксить баги на сверх широких экранах

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
+
   direction: 'horizontal',
   loop: true,
   parallax: true,
@@ -13,20 +13,4 @@ const swiper = new Swiper('.swiper', {
   keyboard: {
     enabled: true,
   },
-
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
